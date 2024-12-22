3b7904086c0a42ced2641f718569b4ba70f7a850
Marina Yaskova <114955877+yaskova@users.noreply.github.com>
2024-12-22 23:13:06 +0300

    Add files via upload

--- hw1/plot/src/plot.py
+++ hw1/plot/src/plot.py
@@ -1,3 +1,4 @@
+print(f"Запуск plot")
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,9 +8,11 @@ import time
 # Путь к директориям логов
 log_file = "./logs/metric_log.csv"
 output_image = "./logs/error_distribution.png"
+print(f"Заданы пути к директориям")
 
 # Убедимся, что папка для логов существует
 os.makedirs("./logs", exist_ok=True)
+print(f"Папка логов существует")
 
 def plot_error_distribution():
     while True:
@@ -18,28 +21,30 @@ def plot_error_distribution():
             if os.path.exists(log_file):
                 # Читаем данные из metric_log.csv
                 df = pd.read_csv(log_file)
-
+                print(f"Данные прочитаны")
                 # Проверяем, есть ли данные для построения
                 if 'absolute_error' in df.columns and not df.empty:
+                    print(f"В файле есть данные")
                     # Построение гистограммы абсолютных ошибок
                     plt.figure(figsize=(8, 6))
                     sns.histplot(
                         df["absolute_error"], 
                         bins=20, 
-                        kde=False, 
-                        color="skyblue", 
-                        edgecolor="black", 
+                        # kde=False, 
+                        kde=True, 
+                        # color="skyblue", 
+                        # edgecolor="black", 
                         label="Гистограмма"
                     )
 
-                    # Добавление линии распределения
-                    sns.kdeplot(
-                        data= df, 
-                        x = "absolute_error", 
-                        color="red", 
-                        linewidth=2, 
-                        label="Линия распределения"
-                    )
+                    # # Добавление линии распределения
+                    # sns.kdeplot(
+                    #     data= df, 
+                    #     x = "absolute_error", 
+                    #     color="red", 
+                    #     linewidth=2, 
+                    #     label="Линия распределения"
+                    # )
                     plt.title("Распределение абсолютных ошибок", fontsize=16)
                     plt.xlabel("Абсолютная ошибка", fontsize=12)
                     plt.ylabel("Частота", fontsize=12)
